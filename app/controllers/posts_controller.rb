be6f0d833deeed2567c9b6327f380304446927de
Ayato Seto <seto_rarihoo1672@icloud.com>
2020-05-08 03:43:23 +0900

    Create "dynamic post page"

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,35 @@
 class PostsController < ApplicationController
+  before_action :authenticate_user!, except: [:index, :show]
   def index
+    @posts = Post.all
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    Post.create(post_params)
+    redirect_to root_path
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    post = Post.find(params[:id])
+    post.update(post_params)
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+
+
+  private
+  def post_params
+    params.require(:post).permit(:title, :content).merge(user_id: current_user.id)
   end
 end
