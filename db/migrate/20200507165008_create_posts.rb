fffd24b4b92c400d0127ee521e87cfb1b8a22168
Ayato Seto <seto_rarihoo1672@icloud.com>
2020-05-08 02:05:14 +0900

    Exec: "rails db:migrate" posts.rb

--- db/migrate/20200507165008_create_posts.rb
+++ db/migrate/20200507165008_create_posts.rb
@@ -1,7 +1,9 @@
 class CreatePosts < ActiveRecord::Migration[5.2]
   def change
     create_table :posts do |t|
-
+      t.string :title, null: false
+      t.text :content, null: false, unique: true
+      t.references  :user, foreign_key: false
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_07_141528) do
+ActiveRecord::Schema.define(version: 2020_05_07_165008) do
+
+  create_table "posts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "title", null: false
+    t.text "content", null: false
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
