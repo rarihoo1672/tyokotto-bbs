52ec22601f36965ad170d89034bd866d99b2ce6d
Ayato Seto <seto_rarihoo1672@icloud.com>
2020-05-08 19:33:21 +0900

    Set "validate maximum"

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
           :recoverable, :rememberable, :validatable
 
   validates :name,  presence: true, uniqueness: true,
-                    length: {maximum: 10 }
+                    length: {maximum: 15 }
 
   has_many :posts
 end
