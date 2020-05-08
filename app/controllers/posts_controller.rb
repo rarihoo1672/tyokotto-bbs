073e4a3047f24e5003a8da30a414201c811987ce
Ayato Seto <seto_rarihoo1672@icloud.com>
2020-05-08 10:02:15 +0900

    Create "post of edit and show page"

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,7 @@
 class PostsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
   def index
-    @posts = Post.all
+    @posts = Post.all.includes(:user)
   end
 
   def new
@@ -20,6 +20,7 @@ class PostsController < ApplicationController
   def update
     post = Post.find(params[:id])
     post.update(post_params)
+    redirect_to root_path
   end
 
   def show
