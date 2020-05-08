a9712c3c125867e6216992fca98e44927847a8eb
Ayato Seto <seto_rarihoo1672@icloud.com>
2020-05-08 15:27:31 +0900

    Add "paging with kaminari"

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
   def show
     user = User.find(params[:id])
     @name = user.name
-    @posts = user.posts.order("created_at DESC")
+    @posts = user.posts.order("created_at DESC").page(params[:page])
   end
 end
 
