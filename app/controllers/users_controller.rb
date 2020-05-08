3346992afc354ade63d15acb23be0622b968ab83
Ayato Seto <seto_rarihoo1672@icloud.com>
2020-05-08 11:59:14 +0900

    Created "basic posts function"

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
   def show
     user = User.find(params[:id])
-    @name = current_user.name
+    @name = user.name
     @posts = user.posts
   end
 end
