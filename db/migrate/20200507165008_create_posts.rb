2be1f17787dbf5c45440a2db002029e7ee6ea872
Ayato Seto <seto_rarihoo1672@icloud.com>
2020-05-09 14:16:59 +0900

    Create "comment model"

--- db/migrate/20200507165008_create_posts.rb
+++ db/migrate/20200507165008_create_posts.rb
@@ -3,7 +3,7 @@ class CreatePosts < ActiveRecord::Migration[5.2]
     create_table :posts do |t|
       t.string :title, null: false
       t.text :content, null: false, unique: true
-      t.references  :user, foreign_key: false
+      t.references  :user, foreign_key: true
       t.timestamps
     end
   end
