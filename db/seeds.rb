0b0f24ebaa26e9763f27ed1e0747e03b107921c6
Ayato Seto <seto_rarihoo1672@icloud.com>
2020-05-11 01:01:52 +0900

    Add "news api"

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,11 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'net/http'
+
+
+news_api_key = ENV["NEWS_API_KEY_ID"]
+uri1 = URI.parse("http://newsapi.org/v2/top-headlines?country=jp&apiKey=#{news_api_key}")
+json1= Net::HTTP.get(uri1)
+article_to_rb = JSON.parse(json1)
+articles = article_to_rb["articles"]
+articles.each do |data|
+  Article.create(title: data["title"], description: data["description"], url: data["url"], urlToImage: data["urlToImage"], publishedAt:data["publishedAt"])
+end
