cbb7127065d9b3d5ecb57c72b6af470aaa97f2ea
Ayato Seto <seto_rarihoo1672@icloud.com>
2020-05-11 00:22:57 +0900

    Create "article model and table"

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_10_024137) do
+ActiveRecord::Schema.define(version: 2020_05_10_152138) do
+
+  create_table "articles", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "title", null: false
+    t.text "description", null: false
+    t.text "url", null: false
+    t.string "urlToImage", null: false
+    t.datetime "publishedAt", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "comments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "text", null: false
