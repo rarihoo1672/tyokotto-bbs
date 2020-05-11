9b1350549ca719c1c10bc4b3856b7765fa581016
Ayato Seto <seto_rarihoo1672@icloud.com>
2020-05-12 02:16:02 +0900

    Set "user posts count"

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,8 @@ class UsersController < ApplicationController
   def show
     user = User.find(params[:id])
     @name = user.name
-    @count = User.count
+    @count = user.posts.count
+
     @posts = user.posts.order("created_at DESC").page(params[:page]).per(5)
   end
 end
