4ebbb33c8179a2eacbaaea48174c132c6228fc24
Ayato Seto <seto_rarihoo1672@icloud.com>
2020-05-14 12:44:06 +0900

    Set "pagenation layout"

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,7 @@
 class PostsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
   def index
-    @posts = Post.all.includes(:user).order("created_at DESC").page(params[:page]).per(5)
+    @posts = Post.all.includes(:user).order("created_at DESC").page(params[:page]).per(20)
     @tags = Tag.all
   end
 
