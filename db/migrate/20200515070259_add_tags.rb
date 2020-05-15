3dfd7d97f06af6502f6d40f261401d9b4395f3ce
Ayato Seto <seto_rarihoo1672@icloud.com>
2020-05-15 18:28:16 +0900

    Change "add tag image to not add"

--- db/migrate/20200515070259_add_tags.rb
+++
@@ -1,5 +0,0 @@
-class AddTags < ActiveRecord::Migration[5.2]
-  def change
-    add_column :tags, :image, :string
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_15_070259) do
+ActiveRecord::Schema.define(version: 2020_05_10_155015) do
 
   create_table "articles", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "title", null: false
@@ -48,7 +48,6 @@ ActiveRecord::Schema.define(version: 2020_05_15_070259) do
     t.string "name", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "image"
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
