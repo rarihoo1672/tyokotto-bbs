fdb316da2027716720393908a109f4d1805d1a68
Ayato Seto <seto_rarihoo1672@icloud.com>
2020-05-16 12:23:49 +0900

    Set "refactoring controller"

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,12 +1,15 @@
 class PostsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
+  before_action :tag_find, only: [:new, :edit, :show]
+  before_action :post_find, only: [:edit, :show]
+
+
   def index
     @posts = Post.all.includes(:user).order("created_at DESC").page(params[:page]).per(20)
     @tags = Tag.all
   end
 
   def new
-    @tag = Tag.find(params[:tag_id])
     @post = Post.new
   end
 
@@ -16,42 +19,40 @@ class PostsController < ApplicationController
       redirect_to root_path, notice: "投稿しました"
     else
       redirect_back(fallback_location: root_path)
-      flash[:notice] = "title、textの空欄はできません。タイトルは25文字までです"
+      flash[:notice] = "投稿できませんでした"
     end
   end
 
   def edit
-    @tag = Tag.find(params[:tag_id])
-    @post = Post.find(params[:id])
-    if @post.user_id == current_user.id
-      render controller: :edit
-    else
-      redirect_back(fallback_location: root_path)
-      flash[:notice] = "投稿に失敗しました"
-    end
+    (@post.user_id == current_user.id) ? (render controller: :edit) : (redirect_back(fallback_location: root_path))
   end
 
   def update
     post = Post.find(params[:id])
     if post.update(post_params)
-      redirect_to root_path, notice: "投稿を編集しました"
+      redirect_to tag_post_path, notice: "投稿しました"
     else
       redirect_back(fallback_location: root_path)
-      flash[:notice] = "投稿に失敗しました"
+      flash[:notice] = "投稿できませんでした"
     end
   end
 
   def show
     @comment = Comment.new
-    @post = Post.find(params[:id])
-    @tag = Tag.find(params[:tag_id])
     @comments = @post.comments.includes(:user).order("created_at DESC").page(params[:page]).per(5)
   end
-
 
 
   private
   def post_params
     params.require(:post).permit(:title, :content, :image).merge(user_id: current_user.id, tag_id: params[:tag_id])
+  end
+
+  def tag_find
+    @tag = Tag.find(params[:tag_id])
+  end
+
+  def post_find
+    @post = Post.find(params[:id])
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,6 @@
 class UsersController < ApplicationController
   def show
     user = User.find(params[:id])
-    @name = user.name
-    @count = user.posts.count
-
     @posts = user.posts.order("created_at DESC").page(params[:page]).per(5)
   end
 end
