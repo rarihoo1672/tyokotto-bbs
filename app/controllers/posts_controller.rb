3a829e112475799ff47529b1611a46bc440d0782
Ayato Seto <seto_rarihoo1672@icloud.com>
2020-05-18 05:02:32 +0900

    Change "comments order desc to asc"

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -39,7 +39,7 @@ class PostsController < ApplicationController
 
   def show
     @comment = Comment.new
-    @comments = @post.comments.includes(:user).order("created_at DESC").page(params[:page]).per(5)
+    @comments = @post.comments.includes(:user).page(params[:page])
   end
 
 
