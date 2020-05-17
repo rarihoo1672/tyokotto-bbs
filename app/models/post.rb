1a158579ee1bcb65dbb2eded609f8502fed5dc02
Ayato Seto <seto_rarihoo1672@icloud.com>
2020-05-17 17:46:14 +0900

    Change "post title length"

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,7 +3,7 @@ class Post < ApplicationRecord
   belongs_to :tag
   has_many :comments
 
-  validates :title, presence: true, length: { maximum: 25}
+  validates :title, presence: true, length: { maximum: 35}
   validates :content, presence: true, uniqueness: true
 
   mount_uploader :image, ImageUploader
