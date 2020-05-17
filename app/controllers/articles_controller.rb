31ff57b3f5189737bf64cc5b5aeb735b4cdb144d
Ayato Seto <seto_rarihoo1672@icloud.com>
2020-05-18 06:06:16 +0900

    Remove "unnecessary articles action"

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,8 +3,4 @@ class ArticlesController < ApplicationController
     @tags = Tag.all
     @articles = Article.all.page(params[:page]).per(8)
   end
-
-  def show
-    @article = Article.find(params[:id])
-  end
 end
